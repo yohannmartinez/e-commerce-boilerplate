45e1070c66fab8120990a4a5466a49c78d9c6a62
MARTINEZ Yohann <yohann.martinez@radiofrance.com>
2025-01-08 16:44:00 +0100

    🔧 alias for lib and css configuration 🍱 add fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,34 @@
+import { fontFamily } from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				border: 'hsl(var(--border) / <alpha-value>)',
+				primary: {
+					DEFAULT: 'hsl(var(--primary) / <alpha-value>)',
+					foreground: 'hsl(var(--primary-foreground) / <alpha-value>)',
+					darker: 'hsl(var(--primary-darker) / <alpha-value>)',
+					clearer: 'hsl(var(--primary-clearer) / <alpha-value>)'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary) / <alpha-value>)',
+					foreground: 'hsl(var(--secondary-foreground) / <alpha-value>)'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			fontFamily: {
+				inter: ['Inter', ...fontFamily.sans],
+				bigola: ['BigolaDisplay', ...fontFamily.sans]
+			}
+		}
 	},
 
 	plugins: []
